6cbb79a8f529aa815713c76a31792669fda75507
therealBudder <80362332+therealBudder@users.noreply.github.com>
2024-11-11 12:18:16 +0000

    return float to gui

--- Simple_Interpreter_Fs/Simple_Interpreter_Fs/Program.fs
+++ Simple_Interpreter_Fs/Simple_Interpreter_Fs/Program.fs
@@ -288,7 +288,7 @@ let testInputs =
 let guiIntegration (inputString: string) = 
     let oList = lexer inputString
     let Out = parseNeval oList
-    snd Out
+    number.fltVal(snd Out)
         
     
 
